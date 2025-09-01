a7731518f685b4fcdad50f5797f64578b7d03aa7
lukaloffert <lukaloeffert@outlook.com>
2025-09-01 14:27:39 +0200

    start of new project. phase 1: product functionality

---
+++ src/main/java/Shelfy/Shelfy_V1/ShelfyV1Application.java
@@ -0,0 +1,13 @@
+package Shelfy.Shelfy_V1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfyV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfyV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/Shelfy/Shelfy_V1/ShelfyV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Shelfy.Shelfy_V1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfyV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
