4b46264d95cb75e7484309d84b72ba66a3b69a3d
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-09-02 14:19:51 +0200

    Added configuration class WebConfig.java in order to be able to run 2 localhost servers simultaneously.

---
+++ src/main/java/Shelfy/Shelfy_V1/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package Shelfy.Shelfy_V1.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // your frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
