44bec0e65f87ac74fe183e3ac39e21bd9856b751
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-09-02 14:26:36 +0200

    Default loading values that can be added to the database for testing. createdAt and lastUpdate are null.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO product (name, brand, unit, default_price, image_url, is_active) VALUES
+                                                                      ('Dish Soap', 'ShinyClean', 'bottle', 2.49, 'https://m.media-amazon.com/images/I/61K3uVSnRZL._UF894,1000_QL80_.jpg', true),
+                                                                      ('Toilet Paper', 'SoftRoll', 'pack', 3.99, 'https://m.media-amazon.com/images/I/81Ml0P+qqnL._UF1000,1000_QL80_.jpg', true),
+                                                                      ('Shampoo', 'SilkHair', 'bottle', 4.50, 'https://m.media-amazon.com/images/I/61ptvFZ1AXL._UF894,1000_QL80_.jpg', true),
+                                                                      ('Toothpaste', 'FreshSmile', 'tube', 2.99, 'https://m.media-amazon.com/images/I/612MXu5fFoL._UF1000,1000_QL80_.jpg', true),
+                                                                      ('Hand Soap', 'PureHands', 'bottle', 1.99, 'https://m.media-amazon.com/images/I/61RhPCXiBNL._UF1000,1000_QL80_.jpg', true),
+                                                                      ('Cooking Oil', 'GoldenDrop', 'bottle', 6.75, 'https://m.media-amazon.com/images/I/71NA0682xSL._UF1000,1000_QL80_.jpg', true),
+                                                                      ('Rice', 'LongGrain', 'bag', 10.50, 'https://m.media-amazon.com/images/I/71o5W8n7-7L._UF1000,1000_QL80_.jpg', true),
+                                                                      ('Milk', 'FarmFresh', 'carton', 1.25, 'https://m.media-amazon.com/images/I/516a94LEw+L._UF1000,1000_QL80_.jpg', true),
+                                                                      ('Bread', 'DailyBake', 'loaf', 1.80, 'https://m.media-amazon.com/images/I/8168t8sVeiL._UF1000,1000_QL80_.jpg', true);
