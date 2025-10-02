8b8a3ece2da35601c58218a14458e76c76ff4b38
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-02 14:50:14 +0200

    Cors configuration

--- src/main/java/Shelfy/Shelfy_V1/configuration/CorsConfig.java
+++ src/main/java/Shelfy/Shelfy_V1/configuration/CorsConfig.java
@@ -15,9 +15,9 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         var cors = new CorsConfiguration();
         cors.setAllowedOrigins(List.of("http://localhost:3000"));
-        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         cors.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-        cors.setAllowCredentials(false); // true only if you really need cookies
+        cors.setAllowCredentials(true);
         var source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", cors);
         return source;
