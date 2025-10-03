adc8686f815e71fb3232b59379b5705c61aa7038
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-04 00:48:38 +0200

    Allowed Get-Requests for the auth/me endpoint.

--- src/main/java/Shelfy/Shelfy_V1/configuration/SecurityConfig.java
+++ src/main/java/Shelfy/Shelfy_V1/configuration/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/auth/me").permitAll()
                         .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
                         .anyRequest().authenticated()
                 )
