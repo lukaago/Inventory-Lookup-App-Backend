e3e11263a85a336cfe45494eaa01d6189ae0ea35
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-09 00:22:32 +0200

    Modified filter to look for cookies if there is no Bearer token.

--- src/main/java/Shelfy/Shelfy_V1/filters/JwtAuthenticationFilter.java
+++ src/main/java/Shelfy/Shelfy_V1/filters/JwtAuthenticationFilter.java
@@ -13,7 +13,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpHeaders;
 
@@ -31,14 +33,34 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
+        String token = null;
         String header = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (header != null && header.startsWith("Bearer ")) {
-            String token = header.substring(7);
+            token = header.substring(7);
+        } else if (request.getCookies() != null) {
+            for (var cookie : request.getCookies()) {
+                if ("accessToken".equals(cookie.getName())) {
+                    token = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (token != null) {
             try {
                 var jws = jwtService.parse(token);
                 var body = jws.getPayload();
                 String username = body.getSubject();
-                var authorities = List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+                @SuppressWarnings("unchecked")
+                List<String> roles = body.get("roles", List.class); // kommt als List<?> aus dem JWT
+
+                // explizit typisieren, damit es zu UsernamePasswordAuthenticationToken passt
+                List<SimpleGrantedAuthority> authorities =
+                        (roles == null)
+                                ? Collections.<SimpleGrantedAuthority>emptyList()
+                                : roles.stream()
+                                .map(SimpleGrantedAuthority::new)
+                                .collect(Collectors.toList());
                 var auth = new UsernamePasswordAuthenticationToken(username, null, authorities);
                 SecurityContextHolder.getContext().setAuthentication(auth);
             } catch (JwtException ex) {
